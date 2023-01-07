364a819964f856bf4777b74160986c630b507a17
Betsegaw Sebsibe <betsesebe@gmail.com>
2023-01-07 09:22:16 +0300

    Add mobile view

--- src/component/sidenav/sidenav.jsx
+++ src/component/sidenav/sidenav.jsx
@@ -16,8 +16,8 @@ function Sidenav() {
   ];
 
   return (
-    <aside className="flex flex-col items-center bg-white text-gray-700 shadow h-full lg:w-56 lg:items-start lg:overflow-hidden">
-      <div className="h-16 flex items-center w-full shadow-md lg:rounded-b-3xl">
+    <aside className="flex flex-col items-center bg-white text-gray-700 shadow h-full md:w-50 lg:w-56 md:items-start md:overflow-hidden">
+      <div className="h-16 flex items-center w-full shadow-md md:rounded-b-3xl">
         <a className="h-6 w-6 mx-auto" href=".">
           <img
             className="h-6 w-6 mx-auto"
@@ -26,19 +26,19 @@ function Sidenav() {
           />
         </a>
       </div>
-      <ul className="w-full lg:mt-2">
+      <ul className="w-full mt-2">
         {navItems.map((element) => (
           <li className="hover:bg-gray-100" key={element.icon}>
             <NavLink
               to={element.link}
-              className="h-16 px-6 flex justify-center lg:justify-start  items-center lg:gap-5 transform hover:translate-x-2 transition-transform ease-in duration-200 text-gray-500 hover:text-gray-800"
+              className="h-16 px-4 flex justify-center md:justify-start  items-center md:gap-5 transform md:hover:translate-x-2 transition-transform ease-in duration-200 text-gray-500 hover:text-gray-800"
             >
               <img
-                className="h-5 w-5 mx-auto lg:mx-0 font-bold"
+                className="h-5 w-5 mx-auto md:mx-0 font-bold"
                 src={element.icon}
                 alt="svelte logo"
               />
-              <span className="hidden lg:block text-sm font-medium mx-auto lg:mx-0 uppercase ">{element.label}</span>
+              <span className="hidden md:block text-sm font-medium mx-auto md:mx-0 uppercase ">{element.label}</span>
             </NavLink>
           </li>
         ))}
