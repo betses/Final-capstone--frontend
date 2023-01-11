4d2b3d76e7368df0cbb6f67fb865ce1d8474800d
fullstop125 <hassanmoses125@gmail.com>
2023-01-11 20:44:44 +0300

    add transiition and animation

--- src/component/pages/events.jsx
+++ src/component/pages/events.jsx
@@ -31,6 +31,17 @@ export default function Events() {
               class="w-64 h-64 max-w-xs overflow-hidden rounded-lg shadow-md bg-white hover:shadow-xl transition-shadow duration-300 ease-in-out"
             ></div>
           </div>
+          <div class="inline-block px-3">
+            <div
+              class="w-64 h-64 max-w-xs overflow-hidden rounded-lg shadow-md bg-white hover:shadow-xl transition-shadow duration-300 ease-in-out"
+            ></div>
+          </div>
+          <div class="inline-block px-3">
+            <div
+              class="w-64 h-64 max-w-xs overflow-hidden rounded-lg shadow-md bg-white hover:shadow-xl transition-shadow duration-300 ease-in-out"
+            ></div>
+          </div>
+          <div class="inline-block px-3"></div>
     </div>
   );
 }
