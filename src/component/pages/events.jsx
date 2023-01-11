7c25f80541e4401ae96a43fff9d419c06e5fafb4
fullstop125 <hassanmoses125@gmail.com>
2023-01-11 20:42:56 +0300

    hide scroll-bar

--- src/component/pages/events.jsx
+++ src/component/pages/events.jsx
@@ -11,6 +11,15 @@ export default function Events() {
         Example
       </h1>
       <div
+      class="flex overflow-x-scroll pb-10 hide-scroll-bar"
+      >
+        <div
+          class="flex flex-nowrap lg:ml-40 md:ml-20 ml-10 "
+        >
+          <div class="inline-block px-3">
+            <div
+              class="w-64 h-64 max-w-xs overflow-hidden rounded-lg shadow-md bg-white hover:shadow-xl transition-shadow duration-300 ease-in-out"
+            ></div>
     </div>
   );
 }
