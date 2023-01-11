a1c7011feb33e2596e45c228abdfeb6d0e2b5ce3
fullstop125 <hassanmoses125@gmail.com>
2023-01-11 20:41:38 +0300

    create body container

--- src/component/pages/events.jsx
+++ src/component/pages/events.jsx
@@ -4,7 +4,13 @@ import "./events.css"
 export default function Events() {
   return (
     <div>
-      
+      <div class="flex flex-col bg-white m-auto p-auto">
+<h1
+        class="flex py-5 lg:px-20 md:px-10 px-5 lg:mx-40 md:mx-20 mx-5 font-bold text-4xl text-gray-800"
+      >
+        Example
+      </h1>
+      <div
     </div>
   );
 }
