9da732fac69360023ed9022a5c3d9d57c8696322
fullstop125 <hassanmoses125@gmail.com>
2023-01-11 20:43:49 +0300

    add card to events page

--- src/component/pages/events.jsx
+++ src/component/pages/events.jsx
@@ -20,6 +20,17 @@ export default function Events() {
             <div
               class="w-64 h-64 max-w-xs overflow-hidden rounded-lg shadow-md bg-white hover:shadow-xl transition-shadow duration-300 ease-in-out"
             ></div>
+             </div>
+          <div class="inline-block px-3">
+            <div
+              class="w-64 h-64 max-w-xs overflow-hidden rounded-lg shadow-md bg-white hover:shadow-xl transition-shadow duration-300 ease-in-out"
+            ></div>
+          </div>
+          <div class="inline-block px-3">
+            <div
+              class="w-64 h-64 max-w-xs overflow-hidden rounded-lg shadow-md bg-white hover:shadow-xl transition-shadow duration-300 ease-in-out"
+            ></div>
+          </div>
     </div>
   );
 }
