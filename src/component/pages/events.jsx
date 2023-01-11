ab0ce4e45faebcbe87c237b9df8e76ae149bdabc
fullstop125 <hassanmoses125@gmail.com>
2023-01-11 20:45:34 +0300

    add card style and effects

--- src/component/pages/events.jsx
+++ src/component/pages/events.jsx
@@ -43,5 +43,22 @@ export default function Events() {
           </div>
           <div class="inline-block px-3"></div>
     </div>
+    <div
+              class="w-64 h-64 max-w-xs overflow-hidden rounded-lg shadow-md bg-white hover:shadow-xl transition-shadow duration-300 ease-in-out"
+            ></div>
+          </div>
+          <div class="inline-block px-3">
+            <div
+              class="w-64 h-64 max-w-xs overflow-hidden rounded-lg shadow-md bg-white hover:shadow-xl transition-shadow duration-300 ease-in-out"
+            ></div>
+          </div>
+          <div class="inline-block px-3">
+            <div
+              class="w-64 h-64 max-w-xs overflow-hidden rounded-lg shadow-md bg-white hover:shadow-xl transition-shadow duration-300 ease-in-out"
+            ></div>
+          </div>
+        </div>
+      </div>
+</div>
   );
 }
