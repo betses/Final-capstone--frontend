4fb49eddec6b85af0b3fdd6296f0adf8681dcf82
Betsegaw Sebsibe <betsesebe@gmail.com>
2023-01-12 05:31:13 +0300

    Add event redux to add-event component

--- src/component/pages/new_event.jsx
+++ src/component/pages/new_event.jsx
@@ -1,7 +1,34 @@
 /* eslint-disable jsx-a11y/label-has-associated-control */
-import React from 'react';
+import { useState } from 'react';
+import { useDispatch } from 'react-redux';
+import { createEvent } from '../../redux/event/event';
 
 export default function CreateEvent() {
+  const dispatch = useDispatch();
+
+  const [value, setValue] = useState({
+    name: '',
+    image: '',
+    category: '',
+    date: '',
+    time: '',
+    description: '',
+    organizer: '',
+    location: '',
+    price: '',
+  });
+
+  const updateValue = (e) => {
+    setValue({
+      ...value,
+      [e.target.id]: e.target.value,
+    });
+  };
+
+  const submitHandler = () => {
+    dispatch(createEvent(value));
+  };
+
   return (
     <div className="w-full flex justify-center">
       <form className="w-1/2 h-min bg-white p-5 mt-10">
@@ -11,47 +38,48 @@ export default function CreateEvent() {
           Event
         </h1>
         <div className="relative z-0 w-full mb-6 group">
-          <input type="name" name="name" id="name" className="block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none   focus:outline-none focus:ring-0 focus:border-blue-600 peer" placeholder=" " required />
+          <input onChange={updateValue} type="name" name="name" id="name" className="block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none   focus:outline-none focus:ring-0 focus:border-blue-600 peer" placeholder=" " required />
           <label htmlFor="name" className="peer-focus:font-medium absolute text-sm text-gray-500 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:left-0 peer-focus:text-blue-600 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6">Name</label>
         </div>
         <div className="relative z-0 w-full mb-6 group">
-          <input type="text" name="image" id="image" className="block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none   focus:outline-none focus:ring-0 focus:border-blue-600 peer" placeholder=" " required />
+          <input onChange={updateValue} type="text" name="image" id="image" className="block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none   focus:outline-none focus:ring-0 focus:border-blue-600 peer" placeholder=" " required />
           <label htmlFor="image" className="peer-focus:font-medium absolute text-sm text-gray-500 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:left-0 peer-focus:text-blue-600 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6">Image URL</label>
         </div>
         <div className="relative z-0 w-full mb-6 group">
-          <input type="text" name="category" id="category" className="block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none   focus:outline-none focus:ring-0 focus:border-blue-600 peer" placeholder=" " required />
+          <input onChange={updateValue} type="" name="category" id="category" className="block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none   focus:outline-none focus:ring-0 focus:border-blue-600 peer" placeholder=" " required />
           <label htmlFor="category" className="peer-focus:font-medium absolute text-sm text-gray-500 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:left-0 peer-focus:text-blue-600 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6">Catagory</label>
         </div>
         <div className="relative z-0 w-full mb-6 group">
-          <input type="text" name="location" id="location" className="block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none   focus:outline-none focus:ring-0 focus:border-blue-600 peer" placeholder=" " required />
+          <input onChange={updateValue} type="" name="location" id="location" className="block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none   focus:outline-none focus:ring-0 focus:border-blue-600 peer" placeholder=" " required />
           <label htmlFor="location" className="peer-focus:font-medium absolute text-sm text-gray-500 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:left-0 peer-focus:text-blue-600 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6">Location</label>
         </div>
         <div className="grid md:grid-cols-2 md:gap-6">
           <div className="relative z-0 w-full mb-6 group">
-            <input type="text" name="date" id="date" className="block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none   focus:outline-none focus:ring-0 focus:border-blue-600 peer" placeholder=" " required />
+            <input onChange={updateValue} type="text" name="date" id="date" className="block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none   focus:outline-none focus:ring-0 focus:border-blue-600 peer" placeholder=" " required />
             <label htmlFor="date" className="peer-focus:font-medium absolute text-sm text-gray-500 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:left-0 peer-focus:text-blue-600 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6">Date</label>
           </div>
           <div className="relative z-0 w-full mb-6 group">
-            <input type="text" name="time" id="time" className="block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none   focus:outline-none focus:ring-0 focus:border-blue-600 peer" placeholder=" " required />
+            <input onChange={updateValue} type="text" name="time" id="time" className="block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none   focus:outline-none focus:ring-0 focus:border-blue-600 peer" placeholder=" " required />
             <label htmlFor="time" className="peer-focus:font-medium absolute text-sm text-gray-500 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:left-0 peer-focus:text-blue-600 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6">Time</label>
           </div>
         </div>
         <div className="grid md:grid-cols-2 md:gap-6">
           <div className="relative z-0 w-full mb-6 group">
-            <input type="number" name="price" id="price" className="block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none   focus:outline-none focus:ring-0 focus:border-blue-600 peer" placeholder=" " required />
+            <input onChange={updateValue} type="number" name="price" id="price" className="block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none   focus:outline-none focus:ring-0 focus:border-blue-600 peer" placeholder=" " required />
             <label htmlFor="price" className="peer-focus:font-medium absolute text-sm text-gray-500  duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:left-0 peer-focus:text-blue-600 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6">Price</label>
           </div>
           <div className="relative z-0 w-full mb-6 group">
-            <input type="text" name="organizer" id="organizer" className="block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none   focus:outline-none focus:ring-0 focus:border-blue-600 peer" placeholder=" " required />
+            <input onChange={updateValue} type="text" name="organizer" id="organizer" className="block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none   focus:outline-none focus:ring-0 focus:border-blue-600 peer" placeholder=" " required />
             <label htmlFor="organizer" className="peer-focus:font-medium absolute text-sm text-gray-500  duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:left-0 peer-focus:text-blue-600 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6">Company (Organizer)</label>
           </div>
         </div>
         <textarea
           className="w-full px-2 py-3 bg-slate-100 rounded-md focus:outline-2 focus:outline-slate-500 my-2"
+          onChange={updateValue}
           id="description"
           placeholder="Description"
         />
-        <button type="submit" className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Submit</button>
+        <button onClick={submitHandler} type="submit" className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Submit</button>
       </form>
     </div>
   );
--- src/redux/event/event.js
+++ src/redux/event/event.js
@@ -1,17 +1,34 @@
-// const userlocal = JSON.parse(localStorage.getItem('user'));
-// const userID = userlocal.id;
+const userlocal = JSON.parse(localStorage.getItem('user'));
+const userID = userlocal.id;
+const url = `http://127.0.0.1:3000/users/${userID}/events`;
+const ADD_EVENT = 'ADD_EVENT';
+const events = [];
 
-// export const createEvent = (data) => (dispatch) => {
-//   fetch(`http://127.0.0.1:3000/users/${userID}/events`, {
-//     method: 'POST',
-//     headers: { 'Content-Type': 'application/json' },
-//     body: JSON.stringify(data),
-//   }).then(async (response) => {
-//     if (response.status === 201) {
-//       const final = await response.json();
-//       dispatch(signUp(final));
-//     }
-//   });
-// };
+export const addEvent = (payload) => ({
+  type: ADD_EVENT,
+  payload,
+});
 
-// export default createEvent;
+export const createEvent = (data) => (dispatch) => {
+  fetch(url, {
+    method: 'POST',
+    headers: { 'Content-Type': 'application/json' },
+    body: JSON.stringify(data),
+  }).then(async (response) => {
+    if (response.status === 201) {
+      const final = await response.json();
+      dispatch(addEvent(final));
+    }
+  });
+};
+
+const eventReducer = (state = events, action) => {
+  switch (action.type) {
+    case ADD_EVENT:
+      return [action.payload];
+    default:
+      return state;
+  }
+};
+
+export default eventReducer;
