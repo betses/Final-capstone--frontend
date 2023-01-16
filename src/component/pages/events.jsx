6ed29088e156ddda5cf7f0d930af58c4b794a58f
iamyunusali <aliyunus291@gmail.com>
2023-01-16 22:27:29 +0300

    link routing to details page & scrolling carousel

--- src/component/pages/events.jsx
+++ src/component/pages/events.jsx
@@ -6,107 +6,81 @@ import {
   ClockIcon,
 } from '@heroicons/react/24/outline';
 import { useDispatch, useSelector } from 'react-redux';
-import { getEvent } from '../../redux/event/event';
+import Carousel from '@itseasy21/react-elastic-carousel';
 import { Link } from 'react-router-dom';
-import { useState } from 'react';
+import { getEvents } from '../../redux/event/event';
 
 export default function Events() {
-  const [currentPage, setCurrentPage] = React.useState(1);
-  const [postPerPage] = React.useState(3);
   const events = useSelector((store) => store.event);
   const dispatch = useDispatch();
   useEffect(() => {
-    dispatch(getEvent());
+    dispatch(getEvents());
   }, [dispatch]);
 
-  const indexOfLastPost = currentPage * postPerPage;
-  const indexOfFirstPost = indexOfLastPost - postPerPage;
-  const currentPosts = events.slice(indexOfFirstPost, indexOfLastPost);
-  const nextPage = () => {
-    if (currentPage >= events.length / postPerPage) return;
-    setCurrentPage(currentPage + 1);
-  };
-  const prevPage = () => {
-    if (currentPage <= 1) return;
-    setCurrentPage(currentPage - 1);
-  };
+  const breakPoints = [
+    { width: 1, itemsToShow: 1 },
+    { width: 560, itemsToShow: 2 },
+    { width: 750, itemsToShow: 3 },
+    { width: 1200, itemsToShow: 4 },
+  ];
 
   return (
-    <div className="px-10">
-      <h1 className="mt-4 mb-12 text-2xl font-extrabold text-gray-900 p md:text-5xl lg:text-6xl">
-        <span className="text-transparent bg-clip-text bg-gradient-to-r to-[#6ea9f0] from-[#5294e2]">
-          Events
-        </span>
-      </h1>
-      <div className="grid gap-10 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4">
-        <button
-          className="bg-gray-300 hover:bg-gray-400 text-gray-800 font-bold py-2 px-4 rounded-l-full h-9 w-14"
-          type="button"
-          onClick={prevPage}
-        >
-          Prev
-        </button>
-        {currentPosts.map((event) => (
-          <Link to={`/details/${event.id}`}>
-            <div className="flex flex-col bg-white border border-gray-200 rounded-lg shadow-md hover:scale-[102%] transition duration-200 ease-in-out">
-              <div className="relative block shrink-0">
-                <div className="relative w-full pt-[100%] hover:bg-slate-300">
-                  <img
-                    className="absolute inset-0 rounded-t-lg cursor-pointer hover:brightness-75 h-full w-full object-cover"
-                    src={
-                      event.image ||
-                      'https://flowbite.com/docs/images/blog/image-1.jpg'
+    <div className='flex flex-col w-full justify-center content-center m-5'>
+      <Carousel breakPoints={breakPoints} className='flex flex-col gap-5 justify-between'>
+        {events.map((event) => (
+          <Link to={`/details/${event.id}`} key={event.id}className='w-full h-full m-2'>
+          <div className="flex flex-col w-full bg-white border border-gray-200 rounded-lg shadow-md hover:scale-[102%] transition duration-200 ease-in-out">
+               <div className="relative block shrink-0">
+                 <div className="relative w-full pt-[100%] hover:bg-slate-300">
+                     <img
+                      className="absolute inset-0 rounded-t-lg cursor-pointer hover:brightness-75 h-full w-full object-cover"
+                      src={
+                      event.image
+                      || 'https://flowbite.com/docs/images/blog/image-1.jpg'
                     }
-                    alt=""
-                  />
+                      alt=""
+                    />
+                  </div>
+                  <p className="absolute px-3 py-1 text-xs font-semibold bg-white rounded-md left-4 top-4">
+                    $
+                    {event.price}
+                  </p>
                 </div>
-                <p className="absolute px-3 py-1 text-xs font-semibold bg-white rounded-md left-4 top-4">
-                  ${event.price}
-                </p>
+                <div className="flex flex-col items-start flex-1 p-5">
+                  <div className="flex justify-between w-full">
+                      <h5 className="text-xl font-bold tracking-tight text-gray-900 h-14 truncate w-32 ">
+                        {event.name}
+                      </h5>
+                    <div className="flex items-center text-gray-700">
+                      <ClockIcon className="w-4 h-4" />
+                      <span className="pl-1 text-xs font-normal">
+                        {DateTime.fromISO(event.time).toFormat('h:ma')}
+                      </span>
+                    </div>
+                  </div>
+                  <p className="flex-1 mb-3 leading-tight text-sm font-normal text-gray-700 min-h-[100px]">
+                    {event.description}
+                  </p>
+                  <div className="flex justify-between w-full mb-3">
+                    <div className="flex items-center text-gray-400">
+                      <MapPinIcon className="w-5 h-5" />
+                      <span className="text-xs font-normal">
+                        {event.location}
+                      </span>
+                    </div>
+                    <div className="flex items-center text-gray-400">
+                      <CalendarDaysIcon className="w-5 h-5" />
+                      <span className="pl-1 text-xs font-normal">
+                        {event.date}
+                      </span>
+                    </div>
+                  </div>
+                </div>
               </div>
-              <div className="flex flex-col items-start flex-1 p-5">
-                <div className="flex justify-between w-full">
-                  <a href=".">
-                    <h5 className="text-xl font-bold tracking-tight text-gray-900 h-14 truncate w-32 ">
-                      {event.name}
-                    </h5>
-                  </a>
-                  <div className="flex items-center text-gray-700">
-                    <ClockIcon className="w-4 h-4" />
-                    <span className="pl-1 text-xs font-normal">
-                      {DateTime.fromISO(event.time).toFormat('h:ma')}
-                    </span>
-                  </div>
-                </div>
-                <p className="flex-1 mb-3 leading-tight text-sm font-normal text-gray-700 min-h-[100px]">
-                  {event.description}
-                </p>
-                <div className="flex justify-between w-full mb-3">
-                  <div className="flex items-center text-gray-400">
-                    <MapPinIcon className="w-5 h-5" />
-                    <span className="text-xs font-normal">
-                      {event.location}
-                    </span>
-                  </div>
-                  <div className="flex items-center text-gray-400">
-                    <CalendarDaysIcon className="w-5 h-5" />
-                    <span className="pl-1 text-xs font-normal">
-                      {event.date}
-                    </span>
-                  </div>
-                </div>
-              </div>
-            </div>
           </Link>
-        ))}
-        <button
-          className="bg-gray-300 hover:bg-gray-400 text-gray-800 font-bold py-2 px-4 rounded-r-full h-9 w-14"
-          type="button"
-          onClick={nextPage}
-        >
-          Next
-        </button>
-      </div>
+            ))}
+      </Carousel>
+
     </div>
   );
 }
