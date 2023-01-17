2aefe7d7deaac8cbe47eccde41f2394b53b07eb7
fullstop125 <hassanmoses125@gmail.com>
2023-01-18 00:47:44 +0300

    fix the event state issue

--- src/redux/event/event.js
+++ src/redux/event/event.js
@@ -13,57 +13,59 @@ export const addEvent = (payload) => ({
   type: ADD_EVENT,
   payload,
 });
-
-export const getAllEvent = (payload) => ({
-  type: GET_EVENTS,
+​
+export const anEvent = (payload) => ({
+  type: GET_EVENT,
   payload,
 });
 export const allEvents = (payload) => ({
   type: GET_ALL_EVENTS,
   payload,
 });
-
+​
 export const createEvent = (data) => async (dispatch) => {
   const response = await fetch(url, {
-    method: 'POST',
-    headers: { 'Content-Type': 'application/json' },
+    method: "POST",
+    headers: { "Content-Type": "application/json" },
     body: JSON.stringify(data),
   });
 
   if (response.status === 201) {
     const final = await response.json();
     dispatch(addEvent(final));
-    return { message: 'the event has been created', status: true };
+    return { message: "the event has been created", status: true };
   }
-  return { message: 'the event has not been created', status: false };
+  return { message: "the event has not been created", status: false };
 };
-
-const eventurl = 'https://eventifyhub.herokuapp.com/events';
+​
+const eventurl = "https://eventifyhub.herokuapp.com/events";
 export const getEvent = () => async (dispatch) => {
   await fetch(eventurl).then(async (result) => {
     const res = await result.json();
     const me = res.filter((m) => m.user_id === local.id);
-    dispatch(getAllEvent(me));
+    dispatch(allEvents(me));
   });
 };
-
+​
 export const getEvents = () => async (dispatch) => {
   await fetch(eventurl).then(async (result) => {
     const res = await result.json();
     dispatch(allEvents(res));
   });
 };
-
+​
 export const getAnEvent = (id) => async (dispatch) => {
-  await fetch(`https://eventifyhub.herokuapp.com/events/${id}`).then(async (result) => {
-    const res = await result.json();
-    dispatch(allEvents(res));
-  });
+  await fetch(`https://eventifyhub.herokuapp.com/events/${id}`).then(
+    async (result) => {
+      const res = await result.json();
+      dispatch(anEvent(res));
+    }
+  );
 };
-
+​
 export const deleteEvent = (data) => (dispatch) => {
   fetch(`https://eventifyhub.herokuapp.com/events/${data}`, {
-    method: 'DELETE',
+    method: "DELETE",
   }).then(async (response) => {
     if (response.status === 200) {
       dispatch(getEvent());
